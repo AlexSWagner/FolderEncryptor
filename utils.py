3c03b2c6d31eb442185db4feaf6cc0af8d644ecf
AlexSWagner <137518068+AlexSWagner@users.noreply.github.com>
2025-03-06 11:27:26 -0500

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-def validate_passwords(password, confirm_password):
-    if len(password) < 8:
-        return False, "Password must be at least 8 characters long."
-    if password != confirm_password:
-        return False, "Passwords do not match."
-    return True, ""
